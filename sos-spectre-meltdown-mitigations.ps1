c47af35f667221e96bf6a421729be61e9eef3424
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-11-18 06:54:17 +0000

    Update and rename sos-specture-meltdown-mitigations.ps1 to sos-spectre-meltdown-mitigations.ps1

--- sos-spectre-meltdown-mitigations.ps1
+++ sos-spectre-meltdown-mitigations.ps1
@@ -7,7 +7,7 @@ $ErrorActionPreference= 'silentlycontinue'
 Write-Output "Elevating priviledges for this process"
 do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
 
-#####SPECTURE MELTDOWN#####
+#####SPECTRE MELTDOWN#####
 #https://support.microsoft.com/en-us/help/4073119/protect-against-speculative-execution-side-channel-vulnerabilities-in
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name FeatureSettingsOverride -Type DWORD -Value 72 -Force
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name FeatureSettingsOverrideMask -Type DWORD -Value 3 -Force
